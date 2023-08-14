7a04b5e6cba4e60199249abcaf6116459e47e49f
Smilecornerstone <adelekekayodesamuel@gmail.com>
2023-08-14 20:07:19 +0000

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,7 +6,6 @@ new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             new_list.append(True)
-
         else:
             new_list.append(False)
     return new_list
